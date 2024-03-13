ab301015aa7a771a8dc0cef238dfe53803df8f80
Rohan Shetty <rshetty449@gmail.com>
2024-03-13 13:17:18 -0400

    add spotless

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
