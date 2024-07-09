927df202532607fd065d4c362f06eac056f891d1
Werner Dietl <wdietl@gmail.com>
2024-07-09 18:08:18 -0400

    Spotless formatting update

--- src/test/java/conformance/NullnessJSpecifyConformanceTest.java
+++ src/test/java/conformance/NullnessJSpecifyConformanceTest.java
@@ -8,7 +8,6 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
